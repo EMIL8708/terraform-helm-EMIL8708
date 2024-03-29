610b99941f1e2293bfe38e8b61dcc191383885be
EMIL8708 <moldoisaem@gmail.com>
2024-03-29 18:27:02 -0500

    added helm chart and terraform with code

--- module/main.tf
+++ module/main.tf
@@ -1,3 +1,10 @@
-module "helm" {
-  source = "./app-cluster"
+module "helm-nginx" {
+  source = "../"
+
+  app_name = "nginx"
+  namespace = "nginx-app"
+
+  cpu_max = "300m"
+  mem_max = "1024Mi"
+
 }
